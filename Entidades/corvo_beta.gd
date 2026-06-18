b2d8577d471f448162667ecc53f078cd623f2d5b
Aramaki-nicolas <nicolasacc2000@gmail.com>
2026-06-18 12:10:25 -0300

    Movimentação: esquerda, direita, mudar mundo, interagir, inventario

--- Entidades/corvo_beta.gd
+++ Entidades/corvo_beta.gd
@@ -7,12 +7,12 @@ var speed:= 50
 
 func get_input():
 	direction_x = Input.get_axis("esquerda","direita")
-	if Input.is_action_just_pressed("pulo"):
-		velocity.y = -jump_strenght
-func apply_gravity(delta):
-	velocity.y +=gravity * delta
+	if Input.is_action_just_pressed("interagir"):
+		print("interagiu")
+	if Input.is_action_just_pressed("AbrirInventario"):
+		print("Abriu")
 func _physics_process(delta: float) -> void:
-	apply_gravity(delta)
+	
 	get_input()
 	
 	velocity.x = direction_x * speed
